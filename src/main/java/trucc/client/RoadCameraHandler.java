756d9649e05682e5cb60001a83f0acb145a5a0b2
Marco Rebhan <me@dblsaiko.net>
2021-06-27 17:14:05 +0200

    Move a lot of road builder stuff into RoadBuilderScreen

--- src/main/java/trucc/client/RoadCameraHandler.java
+++ src/main/java/trucc/client/RoadCameraHandler.java
@@ -3,109 +3,57 @@ package trucc.client;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.Perspective;
 import net.minecraft.text.LiteralText;
+
+import trucc.client.gui.screen.RoadBuilderScreen;
 import trucc.client.render.TransformationManager;
 import trucc.entity.RoadCameraEntity;
 
 public class RoadCameraHandler {
-    public static boolean isCamera = false;
     public static float speed = 2;
     public static int zoom = 50;
     public static int minZoom = 70;
     public static int maxZoom = 30;
-    public static double scrollDelta = 0;
     private static Perspective prevPerspective = null;
-    private static RoadCameraEntity cameraEntity = null;
+    public static RoadCameraEntity cameraEntity = null;
     private static MinecraftClient client = MinecraftClient.getInstance();
 
     public static void startCamera() {
-        if (!isCamera) {
+        if (cameraEntity == null) {
             RoadCameraEntity entity = new RoadCameraEntity(client.world, client.player);
             MinecraftClient.getInstance().setCameraEntity(entity);
             TransformationManager.RemoteCallables.enableIsometricView(50);
-            isCamera = true;
             cameraEntity = entity;
 
             prevPerspective = client.options.getPerspective();
             client.options.setPerspective(Perspective.THIRD_PERSON_BACK);
             client.world.spawnEntity(entity);
-
-            client.mouse.unlockCursor();
         }
     }
 
     public static void stopCamera() {
-        if (isCamera) {
+        if (cameraEntity != null) {
             TransformationManager.RemoteCallables.disableIsometricView();
             MinecraftClient.getInstance().setCameraEntity(client.player);
             cameraEntity.kill();
-            isCamera = false;
             cameraEntity = null;
 
             client.options.setPerspective(prevPerspective);
             prevPerspective = null;
-
-            client.mouse.lockCursor();
         }
     }
 
     public static void tick() {
         while (TruccClient.getInstance().keyBindings.toggleCamera.wasPressed()) {
             client.player.sendMessage(new LiteralText("Toggle Camera was pressed!"), false);
-            if (!isCamera) {
-                startCamera();
-            } else {
-                stopCamera();
-            }
+            client.openScreen(new RoadBuilderScreen());
         }
 
-        if (isCamera) {
-            while (TruccClient.getInstance().keyBindings.rotateCameraLeft.wasPressed()) {
-                if (cameraEntity.getYaw() > 45) {
-                    cameraEntity.setYaw(cameraEntity.getYaw() - 90);
-                } else {
-                    cameraEntity.setYaw(315);
-                }
-            }
+        if (client.currentScreen instanceof RoadBuilderScreen s) {
+            s.worldTick();
+        }
 
-            while (TruccClient.getInstance().keyBindings.rotateCameraRight.wasPressed()) {
-                if (cameraEntity.getYaw() < 315) {
-                    cameraEntity.setYaw(cameraEntity.getYaw() + 90);
-                } else {
-                    cameraEntity.setYaw(45);
-                }
-            }
-
-            if (scrollDelta == 1) {
-                zoom -= client.options.keySneak.isPressed() ? 10 : 5;
-            } else if (scrollDelta == -1) {
-                zoom += client.options.keySneak.isPressed() ? 10 : 5;
-            }
-            zoom = Math.min(Math.max(zoom, maxZoom), minZoom);
-
-            if (client.options.keyLeft.isPressed()) {
-                speed = 2;
-                cameraEntity.sidewaysSpeed = 1;
-            } else if (client.options.keyRight.isPressed()) {
-                speed = 2;
-                cameraEntity.sidewaysSpeed = -1;
-            }
-
-            if (client.options.keyForward.isPressed()) {
-                if (!client.options.keyRight.isPressed() && !client.options.keyLeft.isPressed())
-                    speed = 3.3F;
-                cameraEntity.forwardSpeed = 1;
-            } else if (client.options.keyBack.isPressed()) {
-                if (!client.options.keyRight.isPressed() && !client.options.keyLeft.isPressed())
-                    speed = 3.3F;
-                cameraEntity.forwardSpeed = -1;
-            }
-
-            if (client.options.keySneak.isPressed()) {
-                speed *= 2;
-            }
-
+        if (cameraEntity != null) {
             cameraEntity.tick();
         }
-        scrollDelta = 0;
     }
 }
---
+++ src/main/java/trucc/client/gui/screen/RoadBuilderScreen.java
@@ -0,0 +1,175 @@
+package trucc.client.gui.screen;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.TranslatableText;
+
+import trucc.client.RoadCameraHandler;
+import trucc.client.TruccClient;
+import trucc.client.init.KeyBindings;
+import trucc.entity.RoadCameraEntity;
+
+public class RoadBuilderScreen extends Screen {
+    private boolean sneakPressed;
+    private boolean forwardPressed;
+    private boolean backPressed;
+    private boolean leftPressed;
+    private boolean rightPressed;
+
+    public RoadBuilderScreen() {
+        super(new TranslatableText("trucc.gui.road_builder"));
+    }
+
+    @Override
+    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+        super.render(matrices, mouseX, mouseY, delta);
+    }
+
+    @Override
+    public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
+        RoadCameraEntity cameraEntity = RoadCameraHandler.cameraEntity;
+        MinecraftClient client = this.client;
+        KeyBindings keyBindings = TruccClient.getInstance().keyBindings;
+
+        if (cameraEntity == null || client == null) {
+            return false;
+        }
+
+        if (super.keyPressed(keyCode, scanCode, modifiers)) {
+            return true;
+        } else if (keyBindings.toggleCamera.matchesKey(keyCode, scanCode)) {
+            this.onClose();
+            return true;
+        } else if (keyBindings.rotateCameraLeft.matchesKey(keyCode, scanCode)) {
+            if (cameraEntity.getYaw() > 45) {
+                cameraEntity.setYaw(cameraEntity.getYaw() - 90);
+            } else {
+                cameraEntity.setYaw(315);
+            }
+
+            return true;
+        } else if (keyBindings.rotateCameraRight.matchesKey(keyCode, scanCode)) {
+            if (cameraEntity.getYaw() < 315) {
+                cameraEntity.setYaw(cameraEntity.getYaw() + 90);
+            } else {
+                cameraEntity.setYaw(45);
+            }
+
+            return true;
+        } else {
+            return this.keyEvent(keyCode, scanCode, true);
+        }
+    }
+
+    @Override
+    public boolean keyReleased(int keyCode, int scanCode, int modifiers) {
+        if (super.keyReleased(keyCode, scanCode, modifiers)) {
+            return true;
+        } else {
+            return this.keyEvent(keyCode, scanCode, false);
+        }
+    }
+
+    private boolean keyEvent(int keyCode, int scanCode, boolean pressed) {
+        MinecraftClient client = this.client;
+
+        if (client == null) {
+            return false;
+        }
+
+        if (client.options.keySneak.matchesKey(keyCode, scanCode)) {
+            this.sneakPressed = pressed;
+        } else if (client.options.keyForward.matchesKey(keyCode, scanCode)) {
+            this.forwardPressed = pressed;
+        } else if (client.options.keyBack.matchesKey(keyCode, scanCode)) {
+            this.backPressed = pressed;
+        } else if (client.options.keyLeft.matchesKey(keyCode, scanCode)) {
+            this.leftPressed = pressed;
+        } else if (client.options.keyRight.matchesKey(keyCode, scanCode)) {
+            this.rightPressed = pressed;
+        } else {
+            return false;
+        }
+
+        return true;
+    }
+
+    private void updateEntitySpeed() {
+        MinecraftClient client = this.client;
+        RoadCameraEntity cameraEntity = RoadCameraHandler.cameraEntity;
+
+        if (client == null || cameraEntity == null) {
+            return;
+        }
+
+        float speed = RoadCameraHandler.speed;
+
+        if (this.leftPressed) {
+            speed = 2;
+            cameraEntity.sidewaysSpeed = 1;
+        } else if (this.rightPressed) {
+            speed = 2;
+            cameraEntity.sidewaysSpeed = -1;
+        }
+
+        if (this.forwardPressed) {
+            if (!this.rightPressed && !this.leftPressed)
+                speed = 3.3F;
+            cameraEntity.forwardSpeed = 1;
+        } else if (this.backPressed) {
+            if (!this.rightPressed && !this.leftPressed)
+                speed = 3.3F;
+            cameraEntity.forwardSpeed = -1;
+        }
+
+        if (this.sneakPressed) {
+            speed *= 2;
+        }
+
+        RoadCameraHandler.speed = speed;
+    }
+
+    public void worldTick() {
+        this.updateEntitySpeed();
+    }
+
+    @Override
+    public boolean mouseScrolled(double mouseX, double mouseY, double amount) {
+        if (super.mouseScrolled(mouseX, mouseY, amount)) {
+            return true;
+        }
+
+        MinecraftClient client = this.client;
+
+        if (client == null) {
+            return false;
+        }
+
+        if (amount == 1) {
+            RoadCameraHandler.zoom -= this.sneakPressed ? 10 : 5;
+        } else if (mouseY == -1) {
+            RoadCameraHandler.zoom += this.sneakPressed ? 10 : 5;
+        }
+
+        RoadCameraHandler.zoom = Math.min(Math.max(RoadCameraHandler.zoom, RoadCameraHandler.maxZoom), RoadCameraHandler.minZoom);
+        return true;
+    }
+
+    @Override
+    protected void init() {
+        RoadCameraHandler.startCamera();
+        super.init();
+    }
+
+    @Override
+    public void removed() {
+        RoadCameraHandler.stopCamera();
+        super.removed();
+    }
+
+    @Override
+    public boolean isPauseScreen() {
+        return false;
+    }
+}
