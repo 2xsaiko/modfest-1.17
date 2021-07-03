0f5089ecdd2d1b553ca9316860b878943b6736b0
Marco Rebhan <me@dblsaiko.net>
2021-07-03 09:58:51 +0200

    Store speed in motor

--- src/main/java/trucc/block/MotorWheelBlock.java
+++ src/main/java/trucc/block/MotorWheelBlock.java
@@ -5,13 +5,19 @@ import net.minecraft.block.BlockEntityProvider;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager.Builder;
+import net.minecraft.text.LiteralText;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 
 import java.util.Collections;
 import java.util.EnumMap;
@@ -20,11 +26,39 @@ import java.util.Map;
 import org.jetbrains.annotations.Nullable;
 import trucc.block.entity.MotorWheelBlockEntity;
 
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
 public class MotorWheelBlock extends HorizontalFacingBlock implements BlockEntityProvider {
     private static final Map<Direction, VoxelShape> SHAPES;
 
     public MotorWheelBlock(Settings settings) {
         super(settings);
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        BlockEntity e = world.getBlockEntity(pos);
+
+        if (player.getStackInHand(hand).isEmpty() && e instanceof MotorWheelBlockEntity entity) {
+            boolean reverse = player.isSneaking();
+
+            float speed = entity.getSpeed();
+
+            if (reverse) {
+                speed -= 0.5f;
+            } else {
+                speed += 0.5f;
+            }
+
+            speed = min(max(-20.0f, speed), 20.0f);
+
+            entity.setSpeed(speed);
+            player.sendMessage(new LiteralText("speed %f".formatted(speed)), true);
+            return ActionResult.SUCCESS;
+        }
+
+        return super.onUse(state, world, pos, player, hand, hit);
     }
 
     @Override
--- src/main/java/trucc/block/entity/MotorWheelBlockEntity.java
+++ src/main/java/trucc/block/entity/MotorWheelBlockEntity.java
@@ -2,12 +2,36 @@ package trucc.block.entity;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.math.BlockPos;
 
 import trucc.Trucc;
 
 public class MotorWheelBlockEntity extends BlockEntity {
+    private float speed;
+
     public MotorWheelBlockEntity(BlockPos pos, BlockState state) {
         super(Trucc.getInstance().blockEntityTypes.motorWheel, pos, state);
+    }
+
+    public float getSpeed() {
+        return this.speed;
+    }
+
+    public void setSpeed(float speed) {
+        this.speed = speed;
+        this.markDirty();
+    }
+
+    @Override
+    public void readNbt(NbtCompound nbt) {
+        super.readNbt(nbt);
+        this.speed = nbt.getFloat("speed");
+    }
+
+    @Override
+    public NbtCompound writeNbt(NbtCompound nbt) {
+        nbt.putFloat("speed", this.speed);
+        return super.writeNbt(nbt);
     }
 }
